931172a8f2adbae41e67cd51f81dd4f36e057c6b
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:27:36 -0600

    Update simpledemo.mjs

--- tutorial/simpledemo.mjs
+++ tutorial/simpledemo.mjs
@@ -1,3 +1,4 @@
+// # by: RETBOT 
 import * as simple2 from './simple2.mjs';
 console.log(simple2.hola());
 console.log(`${simple2.siguiente()} ${simple2.cuadrado()}`);
@@ -7,3 +8,4 @@ console.log(`${simple2.siguiente()} ${simple2.cuadrado()}`);
 console.log(`${simple2.siguiente()} ${simple2.cuadrado()}`);      
 console.log(`${simple2.siguiente()} ${simple2.cuadrado()}`);      
 console.log(simple2.valor);
+// # by: RETBOT 
