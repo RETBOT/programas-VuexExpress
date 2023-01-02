e5fb033b6ed43e497f645b5f9569e08f54d35d39
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:29:21 -0600

    Update ls2.js

--- tutorial/ls2.js
+++ tutorial/ls2.js
@@ -1,12 +1,14 @@
+// # by: RETBOT 
 const fs = require('fs');
 const util = require('util');
 const fs_readdir = util.promisify(fs.readdir);
 
 (async () => {
- var dir = '.';
+ var dir = '.'; // # by: RETBOT 
  if(process.argv[2]) dir = process.argv[2];
  const archivos = await fs_readdir(dir);
 for(let arch of archivos){
  console.log(arch);
 }
 })().catch(err => { console.error(err); });
+// # by: RETBOT 
