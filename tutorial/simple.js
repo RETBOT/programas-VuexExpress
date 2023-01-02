dadda0870f02cb6d48148a19c2ecf9cf5dafb10c
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:27:47 -0600

    Update simple.js

--- tutorial/simple.js
+++ tutorial/simple.js
@@ -1,3 +1,4 @@
+// # by: RETBOT 
 var contador = 0;
 exports.siguiente = function(){ return  ++contador;}
 exports.hola = function(){
@@ -6,6 +7,6 @@ exports.hola = function(){
 /*
 exports.funcion = function(arg,arg1) {. . . };
 module.exports.funcion = function(arg,arg1) { . . .};
-
+// # by: RETBOT 
 exports = function (arg, arg1) { . . . }; 
 */
