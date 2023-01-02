ea709b9c41dde4243e4ab17d8e358ba8a9970ec8
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:35:12 -0600

    Update app.js

--- notes/app.js
+++ notes/app.js
@@ -1,3 +1,4 @@
+// <!-- by: RET BOT  -->
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+// <!-- by: RET BOT  -->
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -37,5 +38,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+// <!-- by: RET BOT  -->
 module.exports = app;
