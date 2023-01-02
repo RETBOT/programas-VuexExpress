c463ed29c964ceef74b2c681e2fceba65ccd42df
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:36:55 -0600

    Update index.js

--- express_app/routes/index.js
+++ express_app/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+// <!-- by: RET BOT  -->
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MI PRIMERA APLICACION BACKEDN CON EXPRESS' });
 });
 
 module.exports = router;
+// <!-- by: RET BOT  -->
