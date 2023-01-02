9ee9e647c83cb9c3d1dbb4c1f8690ed8e9434660
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:33:00 -0600

    Update index.js

--- tutorial-vuex/src/router/index.js
+++ tutorial-vuex/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
-
+// <!-- by: R E T B O T  -->
 Vue.use(Router);
 
 export default new Router({
