9fcb394f8ecdfc5f81f63b9cbdeae7e5b3d26b8f
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:28:34 -0600

    Update modulo2.js

--- tutorial/modulo2.js
+++ tutorial/modulo2.js
@@ -1,6 +1,7 @@
+// # by: RETBOT 
 const util = require('util');
 const A = "Un valor diferente de A";
-const B = "Un valor diferente de B";
+const B = "Un valor diferente de B"; // # by: RETBOT 
 const m1 = require('./modulo1.js');
 console.log(`A=${A} B=${B} valores=${util.inspect(m1.valores())}`);
 console.log(`${m1.A} ${m1.B}`)
@@ -8,3 +9,4 @@ const vals = m1.valores();
 vals.B = "Algo completamente diferente";
 console.log(util.inspect(vals));
 console.log(util.inspect(m1.valores()));
+// # by: RETBOT 
