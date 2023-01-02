71d0f0c806c100457360e861bd379894e39acaeb
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:40:40 -0600

    Update Calificacion.js

--- app_peliculas_oficial/modelos/Calificacion.js
+++ app_peliculas_oficial/modelos/Calificacion.js
@@ -1,10 +1,10 @@
-const mongoose =require('mongoose');
+const mongoose =require('mongoose');// <!-- by: R3T B0T  -->
 const Esquema = mongoose.Schema;
 const EsquemaCalificacion = new Esquema({
   pelicula_id: String,
   usuario_id: String,
   calificacion: Number,
 });
-
+// <!-- by: R3T B0T  -->
 const Calificacion = mongoose.model("Calificacion", EsquemaCalificacion);
 module.exports = Calificacion;
